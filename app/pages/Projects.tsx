375a2931f7ad7153c240e98fcdffc7d0f0065563
The24Kings <24kingsunite@gmail.com>
2026-06-29 15:23:53 -0700

    set command on card click

--- app/pages/Projects.tsx
+++ app/pages/Projects.tsx
@@ -3,11 +3,13 @@ import { Card } from '../components';
 import { isSectionData } from '../lib';
 
 import './Projects.css';
+import { useSetCommand } from '../context';
 
 export default function Projects() {
     const data = useRouteLoaderData('routes/projects');
     const posts = isSectionData(data) ? data.posts : [];
     const navigate = useNavigate();
+    const setCommand = useSetCommand();
 
     const sorted = [...posts].sort((a, b) =>
         new Date(b.metadata.date).getTime() - new Date(a.metadata.date).getTime()
@@ -19,7 +21,10 @@ export default function Projects() {
                 <Card
                     key={post.slug}
                     info={post.metadata}
-                    onClick={() => navigate(`/projects/${post.slug}`)}
+                    onClick={() => {
+                        setCommand(`cat ${post.slug}`)
+                        navigate(`/projects/${post.slug}`)
+                    }}
                 />
             ))}
         </ul>
