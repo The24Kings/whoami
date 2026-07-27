66fe2d6e3587345f0dba7c96abccdb04af3f0540
The24Kings <24kingsunite@gmail.com>
2026-07-27 14:12:57 -0700

    forgot to remove old files

--- app/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { BreadCrumb } from './BreadCrumb';
-export { Card } from './Card';
-export { Article } from './Article';
-export { Crumb } from './Crumb';
-export { NextPages } from './NextPages';
-
-export type { CrumbProps, CrumbVariants } from './Crumb'
